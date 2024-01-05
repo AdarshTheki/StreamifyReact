57951749ee50aba5595ebb5cdd16bf435a558b35
Adarsh Verma <adarshverma549@gmail.com>
2024-01-05 15:01:07 +0530

    adding the navigating arrows

--- src/Components/Rows/Rows.js
+++ src/Components/Rows/Rows.js
@@ -1,25 +1,45 @@
-import React from "react";
-import Row from "./Row";
-import useFetch from "../../hooks/useFetch";
-import "./Rows.css";
-import Loading from './Loading'
-import NoPoster from "../NotFund/NoPoster";
+import React, { useRef } from 'react';
+import Row from './Row';
+import useFetch from '../../hooks/useFetch';
+import './Rows.css';
+import Loading from './Loading';
+import NoPoster from '../NotFund/NoPoster';
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 
 function Rows({ fetchUrl, show, toggle }) {
-  const { data, loading } = useFetch(fetchUrl);
+    const { data, loading } = useFetch(fetchUrl);
+    const containerRef = useRef(null);
 
-  if(loading || data.message){
-    return <Loading/>
-  }
+    const sideScroll = (direction) => {
+        const scrollAmount = direction === 'left' ? -400 : 400;
+        containerRef.current.scrollBy({
+            left: scrollAmount,
+            behavior: 'smooth',
+        });
+    };
 
-  return (
-    <div className={`row__container ${toggle ? "grid" : "flex"}`}>
-      {!data?.results?.length && <NoPoster/>}
-      {data?.results?.map((movie) => {
-        return <Row key={movie.id} {...movie} show={show} />;
-      })}
-    </div>
-  );
+    if (loading || data.message) {
+        return <Loading />;
+    }
+
+    return (
+        <div className={`row__container ${toggle ? 'grid' : 'flex'}`} ref={containerRef}>
+            {!data?.results?.length && <NoPoster />}
+            {data?.results?.map((movie) => {
+                return <Row key={movie.id} {...movie} show={show} />;
+            })}
+            {!toggle && (
+                <>
+                    <span id='leftArrow' onClick={() => sideScroll('left')}>
+                        <FaAngleLeft />
+                    </span>
+                    <span id='rightArrow' onClick={() => sideScroll('right')}>
+                        <FaAngleRight />
+                    </span>
+                </>
+            )}
+        </div>
+    );
 }
 
 export default Rows;
