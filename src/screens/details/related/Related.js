0c8236c6eb6c4ccc12327561e9fc967e553d8890
Adarsh Verma <adarshverma549@gmail.com>
2024-01-05 15:04:29 +0530

    modify the small changes the features

--- src/screens/details/related/Related.js
+++ src/screens/details/related/Related.js
@@ -1,21 +1,22 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
-import Rows from "../../../Components/Rows/Rows";
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import Rows from '../../../Components/Rows/Rows';
 
 const Related = () => {
-  const location = useLocation();
-  const path = location.pathname.replace("/show", "");
-  const find = location.pathname.match("movie");
-  return (
-    <div>
-      <h2>Similar Show</h2>
-      <Rows fetchUrl={`${path}/similar`} show={!find ? "tv" : "movie"} />
-      <h2>Recommendations</h2>
-      <Rows
-        fetchUrl={`${path}/recommendations`}
-        show={!find ? "tv" : "movie"}
-      />
-    </div>
-  );
+    const location = useLocation();
+    const path = location.pathname.replace('/show', '');
+    const find = location.pathname.match('movie');
+    return (
+        <div>
+            <div style={{ position: 'relative' }}>
+                <h2>Similar Show</h2>
+                <Rows fetchUrl={`${path}/similar`} show={!find ? 'tv' : 'movie'} />
+            </div>
+            <div style={{ position: 'relative' }}>
+                <h2>Recommendations</h2>
+                <Rows fetchUrl={`${path}/recommendations`} show={!find ? 'tv' : 'movie'} />
+            </div>
+        </div>
+    );
 };
 export default Related;
--- src/screens/searchResult/SearchResult.js
+++ src/screens/searchResult/SearchResult.js
@@ -1,102 +1,92 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import Row from "../../Components/Rows/Row";
-import { fetchDataFromAPI } from "../../API";
+import React, { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Row from '../../Components/Rows/Row';
+import { fetchDataFromAPI } from '../../API';
 
 const SearchResult = () => {
-  const { query } = useParams();
-  const [loading, setLoading] = useState(false);
-  const [collection, setCollection] = useState([]);
-  const [page, setPage] = useState(1);
-  const [totalPage, setTotalPage] = useState("");
-  const [sortBy, setSortBy] = useState("popularity");
-  const [mediaType, setMediaType] = useState("movie");
+    const { query } = useParams();
+    const [loading, setLoading] = useState(false);
+    const [collection, setCollection] = useState([]);
+    const [page, setPage] = useState(1);
+    const [totalPage, setTotalPage] = useState('');
+    const [sortBy, setSortBy] = useState('popularity');
+    const [mediaType, setMediaType] = useState('movie');
 
-  const initialDataFetch = async () => {
-    setLoading(true);
-    try {
-      const res = await fetchDataFromAPI(
-        `/search/multi?query=${query}&page=${page}`
-      );
-      setTotalPage(res?.total_pages);
-      setCollection((prev) => {
-        if (page === 1) return res.results;
-        return [...prev, ...res.results];
-      });
-    } catch (error) {
-      console.error("Error Fetching Data: ", error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
+    useEffect(() => {
+        const initialDataFetch = async () => {
+            setLoading(true);
+            try {
+                const res = await fetchDataFromAPI(`/search/multi?query=${query}&page=${page}`);
+                setTotalPage(res?.total_pages);
+                setCollection((prev) => {
+                    if (page === 1) return res.results;
+                    return [...prev, ...res.results];
+                });
+            } catch (error) {
+                console.error('Error Fetching Data: ', error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+        initialDataFetch();
+    }, [page, query]);
 
-  const handleInfiniteScroll = useCallback(async () => {
-    if (totalPage !== page) {
-      const showLoadMore =
-        window.innerHeight + document.documentElement.scrollTop ===
-        document.documentElement.scrollHeight;
-      if (showLoadMore) {
-        setPage((prev) => prev + 1);
-      }
-    }
-  }, [page, totalPage]);
+    const handleInfiniteScroll = useCallback(async () => {
+        if (totalPage !== page) {
+            const showLoadMore =
+                window.innerHeight + document.documentElement.scrollTop ===
+                document.documentElement.scrollHeight;
+            if (showLoadMore) {
+                setPage((prev) => prev + 1);
+            }
+        }
+    }, [page, totalPage]);
 
-  useEffect(() => {
-    initialDataFetch();
-  }, [query, page]);
+    useEffect(() => {
+        window.addEventListener('scroll', handleInfiniteScroll);
+        return () => window.removeEventListener('scroll', handleInfiniteScroll);
+    }, []);
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleInfiniteScroll);
-    return () => window.removeEventListener("scroll", handleInfiniteScroll);
-  }, []);
+    const sortedCollection = [...collection].sort((a, b) => {
+        if (sortBy === 'popularity') {
+            return mediaType === 'tv' ? a.popularity - b.popularity : b.popularity - a.popularity;
+        } else if (sortBy === 'vote_average') {
+            return mediaType === 'tv'
+                ? a.vote_average - b.vote_average
+                : b.vote_average - a.vote_average;
+        } else if (sortBy === 'release_date') {
+            // Assuming release_date is in ISO format (e.g., 'yyyy-mm-dd')
+            return mediaType === 'tv'
+                ? new Date(a.release_date) - new Date(b.release_date)
+                : new Date(b.release_date) - new Date(a.release_date);
+        }
+        return null;
+    });
 
-  const sortedCollection = [...collection].sort((a, b) => {
-    if (sortBy === "popularity") {
-      return mediaType === "tv"
-        ? a.popularity - b.popularity
-        : b.popularity - a.popularity;
-    } else if (sortBy === "vote_average") {
-      return mediaType === "tv"
-        ? a.vote_average - b.vote_average
-        : b.vote_average - a.vote_average;
-    } else if (sortBy === "release_date") {
-      // Assuming release_date is in ISO format (e.g., 'yyyy-mm-dd')
-      return mediaType === "tv"
-        ? new Date(a.release_date) - new Date(b.release_date)
-        : new Date(b.release_date) - new Date(a.release_date);
-    }
-    return null;
-  });
-
-  return (
-    <div className='max-width'>
-      <div className='filterOption'>
-        <label htmlFor='sort'>Filters By</label>
-        <select
-          id='sort'
-          value={sortBy}
-          onChange={(e) => setSortBy(e.target.value)}>
-          <option value='popularity'>Popularity</option>
-          <option value='vote_average'>vote average</option>
-          <option value='release_date'>release date</option>
-        </select>
-        <button onClick={() => setMediaType("tv")}>Tv Show</button>
-        <button onClick={() => setMediaType("movie")}>Movie</button>
-        <p>
-          You search this 🔥<span>{query}</span>
-        </p>
-      </div>
-      {sortedCollection.length === 0 && (
-        <h1 className='sorry'>Sorry, Results not found!</h1>
-      )}
-      <div className='row__container grid'>
-        {sortedCollection?.map((item) => {
-          return <Row key={item?.id} {...item} show='movie' />;
-        })}
-      </div>
-      {!loading && <div className='spinner'></div>}
-    </div>
-  );
+    return (
+        <div className='max-width'>
+            <div className='filterOption'>
+                <label htmlFor='sort'>Filters By</label>
+                <select id='sort' value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+                    <option value='popularity'>Popularity</option>
+                    <option value='vote_average'>vote average</option>
+                    <option value='release_date'>release date</option>
+                </select>
+                <button onClick={() => setMediaType('tv')}>Tv Show</button>
+                <button onClick={() => setMediaType('movie')}>Movie</button>
+                <p>
+                    You search this 🔥<span>{query}</span>
+                </p>
+            </div>
+            {sortedCollection.length === 0 && <h1 className='sorry'>Sorry, Results not found!</h1>}
+            <div className='row__container grid'>
+                {sortedCollection?.map((item) => {
+                    return <Row key={item?.id} {...item} show='movie' />;
+                })}
+            </div>
+            {!loading && <div className='spinner'></div>}
+        </div>
+    );
 };
 
 export default SearchResult;
