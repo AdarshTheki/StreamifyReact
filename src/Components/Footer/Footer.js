d3464622eaf4346c5c7ce816faa9163eda437244
Adarsh Verma <adarshverma549@gmail.com>
2024-01-05 15:00:24 +0530

    modify the footer icons to small

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -13,10 +13,12 @@ const Footer = () => {
     <footer className='footer'>
       <Wrapper>
         <ul className='menuItems'>
+          <li className='menuItem'>Home</li>
+          <li className='menuItem'>Movie</li>
+          <li className='menuItem'>Tv show</li>
+          <li className='menuItem'>user profile</li>
           <li className='menuItem'>Terms Of Use</li>
           <li className='menuItem'>Privacy-Policy</li>
-          <li className='menuItem'>About</li>
-          <li className='menuItem'>Blog</li>
           <li className='menuItem'>FAQ</li>
         </ul>
         <div className='infoText'>
