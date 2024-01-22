70738ea8aa25f55b1eb528386b674dfabfdaf107
Adarsh Verma <adarshverma549@gmail.com>
2024-01-22 11:03:50 +0530

    modify the home banner components

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,94 +1,44 @@
-import React from "react";
-import Skeleton from "react-loading-skeleton";
-import "./Banner.scss";
-import { useSelector } from "react-redux";
-import BannerImageSlider from "./BannerImageSlider";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Skeleton from 'react-loading-skeleton';
+import BannerImageSlider from './BannerImageSlider';
+import './Banner.scss';
 
 function Banner() {
-  const { banner, isLoading, isError } = useSelector((state) => state.banner);
+    const { banner, isLoading, isError } = useSelector((state) => state.banner);
 
-  if (isLoading || isError) {
-    const styles = {
-      position: "relative",
-      minHeight: "400px",
-      width: "80%",
-      paddingTop: "4rem",
-    };
+    if (isLoading || isError) {
+        const styles = {
+            position: 'relative',
+            minHeight: '400px',
+            width: '80%',
+            paddingTop: '4rem',
+        };
+        return (
+            <div style={styles} className='max-width'>
+                <br />
+                <Skeleton baseColor='#222' highlightColor='#333' width='100%' height={30} />
+                <Skeleton baseColor='#222' highlightColor='#333' width='80%' height={20} />
+                <Skeleton baseColor='#222' highlightColor='#333' width='90%' height={30} />
+                <Skeleton baseColor='#222' highlightColor='#333' width='50%' height={10} />
+                <br />
+                <br />
+                <Skeleton baseColor='#222' highlightColor='#333' width='100%' height={20} />
+                <Skeleton baseColor='#222' highlightColor='#333' width='80%' height={10} />
+                <Skeleton baseColor='#222' highlightColor='#333' width='60%' height={10} />
+                <Skeleton baseColor='#222' highlightColor='#333' width='100%' height={20} />
+                <br />
+                <Skeleton baseColor='#222' highlightColor='#333' width='80%' height={30} />
+                <Skeleton baseColor='#222' highlightColor='#333' width='50%' height={30} />
+            </div>
+        );
+    }
+
     return (
-      <div style={styles} className='max-width'>
-        <br />
-        <Skeleton
-          baseColor='#222'
-          highlightColor='#333'
-          width='100%'
-          height={30}
-        />
-        <Skeleton
-          baseColor='#222'
-          highlightColor='#333'
-          width='80%'
-          height={20}
-        />
-        <Skeleton
-          baseColor='#222'
-          highlightColor='#333'
-          width='90%'
-          height={30}
-        />
-        <Skeleton
-          baseColor='#222'
-          highlightColor='#333'
-          width='50%'
-          height={10}
-        />
-        <br />
-        <br />
-        <Skeleton
-          baseColor='#222'
-          highlightColor='#333'
-          width='100%'
-          height={20}
-        />
-        <Skeleton
-          baseColor='#222'
-          highlightColor='#333'
-          width='80%'
-          height={10}
-        />
-        <Skeleton
-          baseColor='#222'
-          highlightColor='#333'
-          width='60%'
-          height={10}
-        />
-        <Skeleton
-          baseColor='#222'
-          highlightColor='#333'
-          width='100%'
-          height={20}
-        />
-        <br />
-        <Skeleton
-          baseColor='#222'
-          highlightColor='#333'
-          width='80%'
-          height={30}
-        />
-        <Skeleton
-          baseColor='#222'
-          highlightColor='#333'
-          width='50%'
-          height={30}
-        />
-      </div>
+        <div>
+            <BannerImageSlider slides={banner} />
+        </div>
     );
-  }
-
-  return (
-    <div>
-      <BannerImageSlider slides={banner} />
-    </div>
-  );
 }
 
 export default Banner;
--- src/Components/Banner/BannerImageSlider.js
+++ src/Components/Banner/BannerImageSlider.js
@@ -1,20 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import BannerDetail from './BannerDetail';
+import React, { useEffect, useRef, useState } from 'react';
 import { FaCircleChevronRight, FaCircleChevronLeft } from 'react-icons/fa6';
+import BannerDetail from './BannerDetail';
 
-const ImageSlider = ({ slides }) => {
+const ImageSlider = ({ slides, time = 5000 }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
+    const sliderContainer = useRef(null);
 
     useEffect(() => {
-        const intervalId = setInterval(() => {
-            setCurrentIndex((prev) => {
-                return prev === slides.length - 1 ? 0 : prev + 1;
-            });
-        }, 5000);
-        return () => {
-            clearInterval(intervalId);
+        const handleInterval = () => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
         };
-    }, [slides.length]);
+        const intervalId = setInterval(handleInterval, time);
+        return () => clearInterval(intervalId);
+    }, [slides.length, time]);
+
+    useEffect(() => {
+        const scrollSlide = () => {
+            sliderContainer.current.scrollTo({
+                left: currentIndex * sliderContainer.current.clientWidth * 0.15,
+                behavior: 'smooth',
+            });
+        };
+        scrollSlide();
+    }, [currentIndex]);
 
     const goToPrevious = () => {
         setCurrentIndex((currentIndex - 1 + slides.length) % slides.length);
@@ -32,24 +40,37 @@ const ImageSlider = ({ slides }) => {
 
     return (
         <div className='slider__styles'>
-            <div className='leftArrow__styles' onClick={goToPrevious}>
-                <FaCircleChevronLeft />
-            </div>
-            <div className='rightArrow__styles' onClick={goToNext}>
-                <FaCircleChevronRight />
-            </div>
-            <div className='dotsContainer__styles'>
-                {slides.map((_, index) => (
-                    <div
-                        key={index}
-                        className={`dots__styles ${currentIndex === index && 'dotsActive'}`}
-                        onClick={() => goToSlide(index)}></div>
-                ))}
-            </div>
-            <div
-                className='slide__styles'
-                style={{ backgroundImage: `url(${imageUrls})` }}>
+            <div className='slide__styles' style={{ backgroundImage: `url(${imageUrls})` }}>
                 <BannerDetail {...slides[currentIndex]} />
+            </div>
+            <div className='slider'>
+                <div className='slider_leftArrow' onClick={goToPrevious}>
+                    <FaCircleChevronLeft />
+                </div>
+                <div className='slider_rightArrow' onClick={goToNext}>
+                    <FaCircleChevronRight />
+                </div>
+                <div className='slider_lists' ref={sliderContainer}>
+                    {slides.map((item, index) => {
+                        const imageItem = item?.backdrop_path
+                            ? `https://image.tmdb.org/t/p/w200/${item?.backdrop_path}`
+                            : '';
+                        return (
+                            <div
+                                key={item?.id}
+                                className={`slider_item ${currentIndex === index && 'active'}`}>
+                                <img
+                                    src={imageItem}
+                                    alt={item?.title}
+                                    key={index}
+                                    className='slider_img'
+                                    onClick={() => goToSlide(index)}
+                                />
+                                <p className='slider_title'>{item?.title}</p>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </div>
     );
